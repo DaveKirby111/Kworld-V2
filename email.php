a1b289d62548e7379ddaf9b78c6df00660c47ea1
David Kirby <david@kinetic.com>
2022-11-16 15:47:22 -0600

    working on email

--- email.php
+++ email.php
@@ -1,8 +1,37 @@
 <?php
 
-$name = $_POST["name"];
-$email = $_POST["email"];
-$subject = $_POST["subject"];
-$message = $_POST["message"];
+$messagesent = false;
+
+    if (isset(
+        $_POST['email']) && 
+        $_POST['email'] != ''
+        ) {
+
+            if ( filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
+
+
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $subject = $_POST['subject'];
+                $message = $_POST['message'];
+
+                $to = "dkirby111@aol.com";
+                $body = " ";
+                
+                
+                $body = $message; 
+
+                mail($to, $email, $body, $subject); 
+
+                echo "Your Email has been sent.";
+
+                $messagesent = true;
+
+            }
+
+}
+
+    
+    
 
 ?>
